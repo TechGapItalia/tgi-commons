271cfbfe6352c331b9bcb78c5d8deb7ebbce76bf
Domenico Reggiori <d.reggiori@techgap.it>
2015-02-25 14:42:39 +0000

    new class MapperWithList which maps Entities with subcollections
    git-svn-id: svn://svnrepo/TGI_Prj/trunk/tgi-commons@38462 26bf3a1b-7a28-0410-87ae-f795cc41a1b7

---
+++ tgi-commons-mapping/src/main/java/it/tgi/common/api/mapper/MapperWithList.java
@@ -0,0 +1,39 @@
+package it.tgi.common.api.mapper;
+
+import it.tgi.common.api.dto.GenericDto;
+import it.tgi.common.api.model.BaseEntity;
+
+/**
+ * A bi-direction {@link Entity} to {@link DTO} converter, implemented using Guava's {@link com.google.common.base.Converter}
+ *
+ * An {@link Entity} having Collections of other {@link Entity}s must extends this class.
+ * 
+ *
+ * @param <Entity> Entity class
+ * @param <DTO>    DTO class
+ * @param <PK>     Identifier class
+ */
+public abstract class MapperWithList<Entity extends BaseEntity<Long>, DTO extends GenericDto<Long>> extends Mapper<Entity, DTO>{
+
+   
+    protected MapperWithList(Class<Entity> entityClazz, Class<DTO> dtoClazz) {
+		super(entityClazz, dtoClazz);
+	
+	}
+
+	protected final void fillDto(Entity e, DTO dto){
+    	this.fillDto(e,dto,false);
+    };
+    
+    /**
+     * Converts a {@link DTO} to the corresponding {@link Entity}.
+     * <p/>
+     * The {@link Entity id is automatically set}
+     *
+     * @param dto source {@link DTO}
+     * @param e   target {@link Entity}
+     * @param withDisabledChildren if true requires to put disabled entities in lists
+     */
+    protected abstract void fillDto(Entity e, DTO dto,boolean withDisabledChildren);
+    
+}
